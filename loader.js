125407e3310cb1face6ca206143a504d12d92f61
Aditya-psr <adityapratapsinghravat30@gmail.com>
2024-04-19 01:57:27 +0530

    Initial commit

---
+++ loader.js
@@ -0,0 +1,11 @@
+// "use client";
+
+// export default function myImageLoader({ src, width, quality }) {
+//   if (
+//     src.startsWith(
+//       "https://nordicapis.com || https://winningfits.com || https://images.pexels.com"
+//     )
+//   )
+//     return src;
+//   return `https://example.com/${src}?w=${width}&q=${quality || 75}`;
+// }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // output: "export",
+  images: {
+    // loader: "custom",
+    // loaderFile: "./loader.js",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "winningfits.com",
+      },
+      {
+        protocol: "https",
+        hostname: "nordicapis.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
